a1d5fd770ac10b62d82f68f5f078b64db7219391
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2024-12-02 22:58:15 +0200

    Script to produce deliverable codebase

---
+++ exercise_1/deliverable.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+######## Run this script from the root folder of GitHub repository ########
+
+FOLDER_NAME="codebase"
+ASSIGNMENT_NAME="exercise_1"
+
+# Move files to deliverable directory
+mkdir ../$FOLDER_NAME
+cp -a . ../$FOLDER_NAME
+
+# Change directory to deliverable
+cd ../$FOLDER_NAME
+
+# Remove unnesesary git files
+rm -rf .git
+rm .gitignore
+rm README.md
+
+rm $ASSIGNMENT_NAME/deliverable.sh
