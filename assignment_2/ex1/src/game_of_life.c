c4cf1b37cfd8760af4f44ca2510b2b853c1deb01
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2024-12-29 03:47:18 +0200

    No Function Call in Parallel Region
    - Use the input and output array to simulate borders.
      - Easier to handle neighbor cases.
    - Do NOT call functions in parallel regions of omp results in
      noticeable speedup, whereas, when subroutines are invoked there
      is a slowdown!

--- assignment_2/ex1/src/game_of_life.c
+++ assignment_2/ex1/src/game_of_life.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#ifdef DEBUG
+#include <unistd.h>
+#define WAIT_US 200000
+#endif
+
 #include "game_of_life.h"
 
 struct game_of_life_s {
@@ -23,7 +28,7 @@ struct game_of_life_s {
      */
     int *output_ptr;
     /*
-     * The size of the grid (or the one dimension of the square matrix).
+     * The edge size of the grid plus the borders.
      */
     int grid;
 };
@@ -34,7 +39,7 @@ int gol_init(game_of_life_t *const gol, const int grid) {
     };
 
     int *input_ptr;
-    if((input_ptr = malloc(grid * grid * sizeof(int))) == NULL) {
+    if((input_ptr = calloc((grid + 2) * (grid + 2), sizeof(int))) == NULL) {
         free(*gol);
         *gol = NULL;
 
@@ -42,7 +47,7 @@ int gol_init(game_of_life_t *const gol, const int grid) {
     };
 
     int *output_ptr;
-    if((output_ptr = malloc(grid * grid * sizeof(int))) == NULL) {
+    if((output_ptr = calloc((grid + 2) * (grid + 2), sizeof(int))) == NULL) {
         free(input_ptr);
 
         free(*gol);
@@ -53,7 +58,7 @@ int gol_init(game_of_life_t *const gol, const int grid) {
 
     (*gol)->input_ptr = input_ptr;
     (*gol)->output_ptr = output_ptr;
-    (*gol)->grid = grid;
+    (*gol)->grid = grid + 2;
 
     return 0;
 }
@@ -80,10 +85,14 @@ int gol_parse_input_from_file(
         return 1;
     }
 
-    int index = 0;
+    int index = (*gol)->grid - 1;
     int cell;
 
     while(fscanf(file, "%d;", &cell) != EOF) {
+        // reached east border
+        if(index % ((*gol)->grid - 1) == 0) {
+            index += 2; // skip west border
+        }
         *((*gol)->input_ptr + index++) = cell;
     }
 
@@ -97,8 +106,8 @@ void gol_random_input(const game_of_life_t *const gol) {
 
     srand(seed);
 
-    for(int i = 0; i < (*gol)->grid; i++) {
-        for(int j = 0; j < (*gol)->grid; j++) {
+    for(int i = 1; i < (*gol)->grid - 1; i++) {
+        for(int j = 1; j < (*gol)->grid - 1; j++) {
             *((*gol)->input_ptr + (*gol)->grid * i + j) = rand() % 2;
         }
     }
@@ -107,6 +116,10 @@ void gol_random_input(const game_of_life_t *const gol) {
 /*
  * Print the cells of the grid.
  *
+ * The alive cells are drawn as 'o', the dead cells are drawn as ' '
+ * and the horizontal and vertical border cells are drawn as '-' and '|',
+ * respectively.
+ *
  * Parameters:
  * - arr_ptr: the matrix.
  * - size: the size of the matrix.
@@ -114,199 +127,15 @@ void gol_random_input(const game_of_life_t *const gol) {
 void print_cells(const int *const arr_ptr, const int size) {
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++)
-            if(*(arr_ptr + size * i + j) == 0)
-                printf(" ");
+            if((i == 0) || (i == size - 1)) {
+                printf("-"); // horizontal border
+            } else if((j == 0) || (j == size - 1)) {
+                printf("|"); // vertical border
+            } else if(*(arr_ptr + size * i + j) == 0)
+                printf(" "); // dead cell
             else
-                printf("o");
+                printf("o"); // alive cell
         printf("\n");
-    }
-}
-
-/*
- * Calculate the number of alive neighbors a cell has in a square matrix.
- *
- * The neighbors are assumed to be the cells that are adjacent to the cell in
- * the north, east, south, and west directions, as well as the diagonals.
- *
- * The values of the cells are either 0 or 1, where 0 represents a dead and 1 an
- * alive cell.
- *
- * Parameters:
- * - input_ptr: the input matrix.
- * - i: the row of the cell.
- * - j: the column of the cell.
- * - size: the size of the matrix.
- *
- * Returns:
- * - the number of neighbors of the cell.
- */
-int gol_calculate_neighbors(
-    const int *const input_ptr, const int i, const int j, const int size
-) {
-    int neighbors = 0;
-
-    int north = i - 1;
-    int east = j + 1;
-    int south = i + 1;
-    int west = j - 1;
-
-    // Follows a branch-friendly approach to check the neighbors
-    if((0 < i) && (i < size - 1) && (0 < j) && (j < size - 1)) {
-        // inner cells
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // north-east
-        neighbors += input_ptr[size * north + east];
-        // east
-        neighbors += input_ptr[size * i + east];
-        // south-east
-        neighbors += input_ptr[size * south + east];
-        // south
-        neighbors += input_ptr[size * south + j];
-        // south-west
-        neighbors += input_ptr[size * south + west];
-        // west
-        neighbors += input_ptr[size * i + west];
-        // north-west
-        neighbors += input_ptr[size * north + west];
-    } else if((i == 0) && (0 < j) && (j < size - 1)) {
-        // first row - excluding corners
-
-        // east
-        neighbors += input_ptr[size * i + east];
-        // south-east
-        neighbors += input_ptr[size * south + east];
-        // south
-        neighbors += input_ptr[size * south + j];
-        // south-west
-        neighbors += input_ptr[size * south + west];
-        // west
-        neighbors += input_ptr[size * i + west];
-    } else if((0 < i) && (i < size - 1) & (j == 0)) {
-        // first column - excluding corners
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // north-east
-        neighbors += input_ptr[size * north + east];
-        // east
-        neighbors += input_ptr[size * i + east];
-        // south-east
-        neighbors += input_ptr[size * south + east];
-        // south
-        neighbors += input_ptr[size * south + j];
-    } else if((i == size - 1) && (0 < j) && (j < size - 1)) {
-        // last row - excluding corners
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // north-east
-        neighbors += input_ptr[size * north + east];
-        // east
-        neighbors += input_ptr[size * i + east];
-        // west
-        neighbors += input_ptr[size * i + west];
-        // north-west
-        neighbors += input_ptr[size * north + west];
-    } else if((0 < i) && (i < size - 1) & (j == size - 1)) {
-        // last column - excluding corners
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // south
-        neighbors += input_ptr[size * south + j];
-        // south-west
-        neighbors += input_ptr[size * south + west];
-        // west
-        neighbors += input_ptr[size * i + west];
-        // north-west
-        neighbors += input_ptr[size * north + west];
-
-    } else if((i == 0) && (j == 0)) {
-        // top-left corner
-
-        // east
-        neighbors += input_ptr[size * i + east];
-        // south-east
-        neighbors += input_ptr[size * south + east];
-        // south
-        neighbors += input_ptr[size * south + j];
-    } else if((i == 0) && (j == size - 1)) {
-        // top-right corner
-
-        // south
-        neighbors += input_ptr[size * south + j];
-        // south-west
-        neighbors += input_ptr[size * south + west];
-        // west
-        neighbors += input_ptr[size * i + west];
-    } else if((i == size - 1) && (j == 0)) {
-        // bottom-left corner
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // north-east
-        neighbors += input_ptr[size * north + east];
-        // east
-        neighbors += input_ptr[size * i + east];
-    } else if((i == size - 1) && (j == size - 1)) {
-        // bottom-right corner
-
-        // north
-        neighbors += input_ptr[size * north + j];
-        // west
-        neighbors += input_ptr[size * i + west];
-        // north-west
-        neighbors += input_ptr[size * north + west];
-    }
-
-    return neighbors;
-}
-
-/*
- * Enforce the rules of the game of life to get the state of a cell in the next
- * generation.
- *
- * The values of the cells are either 0 or 1, where 0 represents a
- * dead and 1 an alive cell.
- *
- * The rules are:
- * 1. Any live cell with fewer than two live neighbors dies, as if by
- * underpopulation.
- * 2. Any live cell with two or three live neighbors lives on to the next
- * generation.
- * 3. Any live cell with more than three live neighbors dies, as if by
- * overpopulation.
- * 4. Any dead cell with exactly three live neighbors becomes a live cell, as if
- * by reproduction.
- *
- * Parameters:
- * - input_ptr: the pointer to the input matrix.
- * - output_ptr: the pointer to the output matrix.
- * - neighbors: the number of neighbors of the cell.
- * - i: the row of the cell.
- * - j: the column of the cell.
- * - size: the size of the matrix.
- */
-void gol_enforce_rules(
-    const int *const input_ptr, int *const output_ptr, const int neighbors,
-    const int i, const int j, const int size
-) {
-    if(*(input_ptr + size * i + j) == 1) {
-        if(neighbors < 2) { // underpopulation
-            *(output_ptr + size * i + j) = 0;
-        } else if(neighbors < 4) {
-            *(output_ptr + size * i + j) = 1;
-        } else { // overpopulation
-            *(output_ptr + size * i + j) = 0;
-        }
-    } else {
-        if(neighbors == 3) { // reproduction
-            *(output_ptr + size * i + j) = 1;
-        } else {
-            *(output_ptr + size * i + j) = 0;
-        }
     }
 }
 
@@ -320,19 +149,40 @@ void gol_execute(const game_of_life_t *const gol, const int generations) {
 #endif
 
     for(int gen = 0; gen < generations; gen++) {
-        for(int i = 0; i < (*gol)->grid; i++) {
-            for(int j = 0; j < (*gol)->grid; j++) {
-                neighbors = gol_calculate_neighbors(
-                    (*gol)->input_ptr, i, j, (*gol)->grid
-                );
-                gol_enforce_rules(
-                    (*gol)->input_ptr, (*gol)->output_ptr, neighbors, i, j,
-                    (*gol)->grid
-                );
+        for(int i = 1; i < (*gol)->grid - 1; i++) {
+            for(int j = 1; j < (*gol)->grid - 1; j++) {
+                // Calculate the number of neighbors
+                neighbors =
+                    (*gol)->input_ptr[(*gol)->grid * (i - 1) + j] +
+                    (*gol)->input_ptr[(*gol)->grid * (i - 1) + (j + 1)] +
+                    (*gol)->input_ptr[(*gol)->grid * i + (j + 1)] +
+                    (*gol)->input_ptr[(*gol)->grid * (i + 1) + (j + 1)] +
+                    (*gol)->input_ptr[(*gol)->grid * (i + 1) + j] +
+                    (*gol)->input_ptr[(*gol)->grid * (i + 1) + (j - 1)] +
+                    (*gol)->input_ptr[(*gol)->grid * i + (j - 1)] +
+                    (*gol)->input_ptr[(*gol)->grid * (i - 1) + (j - 1)];
+
+                // Enforce the rules of the game of life
+                if((*gol)->input_ptr[(*gol)->grid * i + j] == 1) {
+                    if(neighbors < 2) { // underpopulation
+                        (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                    } else if(neighbors < 4) {
+                        (*gol)->output_ptr[(*gol)->grid * i + j] = 1;
+                    } else { // overpopulation
+                        (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                    }
+                } else {
+                    if(neighbors == 3) { // reproduction
+                        (*gol)->output_ptr[(*gol)->grid * i + j] = 1;
+                    } else {
+                        (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                    }
+                }
             }
         }
 
 #ifdef DEBUG
+        usleep(WAIT_US);
         printf("After generation %d:\n", gen);
         print_cells((*gol)->output_ptr, (*gol)->grid);
 #endif
@@ -364,21 +214,42 @@ void gol_execute_parallel(
 
         for(int gen = 0; gen < generations; gen++) {
 #pragma omp for schedule(static)
-            for(int i = 0; i < (*gol)->grid; i++) {
-                for(int j = 0; j < (*gol)->grid; j++) {
-                    neighbors = gol_calculate_neighbors(
-                        (*gol)->input_ptr, i, j, (*gol)->grid
-                    );
-                    gol_enforce_rules(
-                        (*gol)->input_ptr, (*gol)->output_ptr, neighbors, i, j,
-                        (*gol)->grid
-                    );
+            for(int i = 1; i < (*gol)->grid - 1; i++) {
+                for(int j = 1; j < (*gol)->grid - 1; j++) {
+                    // Calculate the number of neighbors
+                    neighbors =
+                        (*gol)->input_ptr[(*gol)->grid * (i - 1) + j] +
+                        (*gol)->input_ptr[(*gol)->grid * (i - 1) + (j + 1)] +
+                        (*gol)->input_ptr[(*gol)->grid * i + (j + 1)] +
+                        (*gol)->input_ptr[(*gol)->grid * (i + 1) + (j + 1)] +
+                        (*gol)->input_ptr[(*gol)->grid * (i + 1) + j] +
+                        (*gol)->input_ptr[(*gol)->grid * (i + 1) + (j - 1)] +
+                        (*gol)->input_ptr[(*gol)->grid * i + (j - 1)] +
+                        (*gol)->input_ptr[(*gol)->grid * (i - 1) + (j - 1)];
+
+                    // Enforce the rules of the game of life
+                    if((*gol)->input_ptr[(*gol)->grid * i + j] == 1) {
+                        if(neighbors < 2) { // underpopulation
+                            (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                        } else if(neighbors < 4) {
+                            (*gol)->output_ptr[(*gol)->grid * i + j] = 1;
+                        } else { // overpopulation
+                            (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                        }
+                    } else {
+                        if(neighbors == 3) { // reproduction
+                            (*gol)->output_ptr[(*gol)->grid * i + j] = 1;
+                        } else {
+                            (*gol)->output_ptr[(*gol)->grid * i + j] = 0;
+                        }
+                    }
                 }
             }
 
 #ifdef DEBUG
 #pragma omp single
             {
+                usleep(WAIT_US);
                 printf("After generation %d:\n", gen);
                 print_cells((*gol)->output_ptr, (*gol)->grid);
             }
