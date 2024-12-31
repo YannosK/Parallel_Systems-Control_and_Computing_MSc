39e83ea96a461d132914840a0473f817f8a6f3ba
Ioannis Koutoulas <ea2300204@linux29.di.uoa.gr>
2024-12-31 12:12:23 +0200

    Run average results for simpler code in Linux DI

--- assignment_2/ex2/src/equations_system.c
+++ assignment_2/ex2/src/equations_system.c
@@ -148,32 +148,55 @@ int back_substitution_by_column(double **A, double *b, double *x, size_t n) {
  distribute
  * mindlessly to threads
  */
+//int back_substitution_by_row_p(
+//    double **A, double *b, double *x, size_t n, unsigned long threadcount
+//) {
+//
+//    size_t row, column;
+//    double local_var = 0.0;
+//    unsigned long internal_iterations;
+//
+//    for(row = n - 1; row < n; row--) {
+//        local_var = b[row];
+//        internal_iterations = row + 1 - n;
+//
+//        if(internal_iterations > threadcount) {
+//#pragma omp parallel for num_threads(threadcount) default(none) reduction(- : local_var) private(column) shared(A, row, x, n) schedule(runtime)
+//            for(column = row + 1; column < n; column++) {
+//                local_var -= A[row][column] * x[column];
+//            }
+//        } else {
+//#pragma omp parallel for num_threads(internal_iterations) default(none) reduction(- : local_var) private(column) shared(A, row, x, n) schedule(runtime)
+//            for(column = row + 1; column < n; column++) {
+//                local_var -= A[row][column] * x[column];
+//            }
+//        }
+//
+//        x[row] = local_var / A[row][row];
+//    }
+//
+//    return 0;
+//}
+/***************************************************************/
+
+/***************************************************************
+ * NOTE:
+ * Simpler version
+ */
 int back_substitution_by_row_p(
     double **A, double *b, double *x, size_t n, unsigned long threadcount
 ) {
 
     size_t row, column;
     double local_var = 0.0;
-    unsigned long internal_iterations;
 
     for(row = n - 1; row < n; row--) {
         local_var = b[row];
-        internal_iterations = row + 1 - n;
 
-        if(internal_iterations > threadcount) {
-#pragma omp parallel for num_threads(threadcount) default(none)                \
-    reduction(- : local_var) private(column) shared(A, row, x, n)              \
-    schedule(runtime)
-            for(column = row + 1; column < n; column++) {
-                local_var -= A[row][column] * x[column];
-            }
-        } else {
-#pragma omp parallel for num_threads(internal_iterations) default(none)        \
-    reduction(- : local_var) private(column) shared(A, row, x, n)              \
-    schedule(runtime)
-            for(column = row + 1; column < n; column++) {
-                local_var -= A[row][column] * x[column];
-            }
+#pragma omp parallel for num_threads(threadcount) default(none) \
+       	reduction(- : local_var) private(column) shared(A, row, x, n) schedule(runtime)
+        for(column = row + 1; column < n; column++) {
+            local_var -= A[row][column] * x[column];
         }
 
         x[row] = local_var / A[row][row];
@@ -185,36 +208,40 @@ int back_substitution_by_row_p(
 
 /***************************************************************
  * NOTE:
- * Simpler version
+ * Code that takes into considaration the number of iterations to not
+ distribute
+ * mindlessly to threads
  */
-// int back_substitution_by_row_p(
-//     double **A, double *b, double *x, size_t n, unsigned long threadcount
-// ) {
-
-//     size_t row, column;
-//     double local_var = 0.0;
-
-//     for(row = n - 1; row < n; row--) {
-//         local_var = b[row];
-
-// #pragma omp parallel for num_threads(threadcount) default(none) reduction(- :
-// local_var) private(column) shared(A, row, x, n) schedule(runtime)
-//         for(column = row + 1; column < n; column++) {
-//             local_var -= A[row][column] * x[column];
-//         }
-
-//         x[row] = local_var / A[row][row];
-//     }
-
-//     return 0;
-// }
+//int back_substitution_by_column_p(
+//    double **A, double *b, double *x, size_t n, unsigned long threadcount
+//) {
+//    long long row, column;
+//
+//#pragma omp parallel for num_threads(threadcount) schedule(runtime)
+//    for(row = 0; row < (long long)n; row++)
+//        x[row] = b[row];
+//
+//    for(column = (long long)n - 1; column >= 0; column--) {
+//        x[column] /= A[column][column];
+//
+//        if((unsigned long)column > threadcount) {
+//#pragma omp parallel for num_threads(threadcount) schedule(runtime)
+//            for(row = 0; row < column; row++)
+//                x[row] -= A[row][column] * x[column];
+//        } else {
+//#pragma omp parallel for num_threads(column) schedule(runtime)
+//            for(row = 0; row < column; row++)
+//                x[row] -= A[row][column] * x[column];
+//        }
+//    }
+//
+//    return 0;
+//}
 /***************************************************************/
 
 /***************************************************************
  * NOTE:
- * Code that takes into considaration the number of iterations to not
- distribute
- * mindlessly to threads
+ * Simpler version
  */
 int back_substitution_by_column_p(
     double **A, double *b, double *x, size_t n, unsigned long threadcount
@@ -227,44 +254,13 @@ int back_substitution_by_column_p(
 
     for(column = (long long)n - 1; column >= 0; column--) {
         x[column] /= A[column][column];
-
-        if((unsigned long)column > threadcount) {
 #pragma omp parallel for num_threads(threadcount) schedule(runtime)
-            for(row = 0; row < column; row++)
-                x[row] -= A[row][column] * x[column];
-        } else {
-#pragma omp parallel for num_threads(column) schedule(runtime)
-            for(row = 0; row < column; row++)
-                x[row] -= A[row][column] * x[column];
-        }
+        for(row = 0; row < column; row++)
+            x[row] -= A[row][column] * x[column];
     }
 
     return 0;
 }
-/***************************************************************/
-
-/***************************************************************
- * NOTE:
- * Simpler version
- */
-// int back_substitution_by_column_p(
-//     double **A, double *b, double *x, size_t n, unsigned long threadcount
-// ) {
-//     long long row, column;
-
-// #pragma omp parallel for num_threads(threadcount) schedule(runtime)
-//     for(row = 0; row < (long long)n; row++)
-//         x[row] = b[row];
-
-//     for(column = (long long)n - 1; column >= 0; column--) {
-//         x[column] /= A[column][column];
-// #pragma omp parallel for num_threads(threadcount) schedule(runtime)
-//         for(row = 0; row < column; row++)
-//             x[row] -= A[row][column] * x[column];
-//     }
-
-//     return 0;
-// }
 /***************************************************************/
 
 /////////////////////////////////////////////////////////////////
