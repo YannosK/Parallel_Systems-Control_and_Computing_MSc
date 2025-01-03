27277a9491aff1da4410eb741dae482f24a16757
YannosK <giannos2405@gmail.com>
2025-01-03 13:02:23 +0200

    Final latex scripts

--- assignment_2/ex2/scripts/table_creator.py
+++ assignment_2/ex2/scripts/table_creator.py
@@ -1,5 +1,6 @@
+import argparse
 import csv
-import sys
+import os
 
 def csv_to_list(filename):
     with open(filename, 'r') as file:
@@ -13,78 +14,107 @@ def string_to_power_of_ten(value: str) -> str:
     exponent = len(value) - 1
     return f"$10^{exponent}$"
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('method', help='The method for back-substitution must either be \'rows\' or \'columns\'')
+
 if __name__ == "__main__":
 
     ###################################
     # Argument check
     ###################################
 
-    if len(sys.argv) != 2:
-        print('Usage: python3 runner.py <tabletype>')
-        print('Table type is either tim for timings or ef for efficiencies')
-        sys.exit(1)
+    args = parser.parse_args()
 
-    tabletype = sys.argv[1]
-
-    if tabletype == 'tim':
-        csv_file = "../timings.csv"
-    elif tabletype == 'ef':
-        csv_file = "../efficiencies.csv"
-    else:
-        raise ValueError('Table type is either tim for timings or ef for efficiencies')
-
-    ###################################
-    # Csv to lists
-    ###################################
-
-    valuelist = csv_to_list(csv_file)
-
-    ###################################
-    # Creating tex blocks
-    ###################################
-
-    # LaTeX template for a single block
-    template_block = """
-    %%%%%%%%%%%%%%%%%%
-    {Iterations}
-    % 1 Thread 
-    & {Val_1}
-    % 2 Threads 
-    & {Val_2}
-    % 3 Threads 
-    & {Val_3}
-    % 4 Threads 
-    & {Val_4}
-    % 5 Threads 
-    & {Val_5}
-    % 6 Threads 
-    & {Val_6}
-    % 7 Threads 
-    & {Val_7}
-    % 8 Threads 
-    & {Val_8}
-    \\\\
-"""
-
-    if tabletype == 'tim':
-        output_filename = "timings_template.tex"
-    elif tabletype == 'ef':
-        output_filename = "efficiencies_template.tex"
+    if args.method not in ['rows', 'columns']:
+        raise ValueError("Incorrect argument")
     
-    with open(output_filename, "w") as tex_file:
-        for value_row in valuelist:
-            tex_block = template_block.format(
-                # Iterations=string_to_power_of_ten(value_row[0]), # choose this for exponent notation
-                Iterations=value_row[0], # choose this for full values
-                Val_1="{:.0f}".format(1000000*float(value_row[1])),
-                Val_2="{:.0f}".format(1000000*float(value_row[2])),
-                Val_3="{:.0f}".format(1000000*float(value_row[3])),
-                Val_4="{:.0f}".format(1000000*float(value_row[4])),
-                Val_5="{:.0f}".format(1000000*float(value_row[5])),
-                Val_6="{:.0f}".format(1000000*float(value_row[6])),
-                Val_7="{:.0f}".format(1000000*float(value_row[7])),
-                Val_8="{:.0f}".format(1000000*float(value_row[8]))
-            ).strip()  # Remove extra newlines at the start and end of the block
-            tex_file.write("\t" + tex_block + "\n")
+    method = args.method
 
-    print(f"LaTeX file '{output_filename}' generated successfully.")
+    ###################################
+    # templates and .tex files
+    ###################################
+
+    if method == 'rows':
+        output_filename = os.path.join('..', 'results', 'latex_tables', 'rows_speedup.tex')
+    elif method == 'columns':
+        output_filename = os.path.join('..', 'results', 'latex_tables', 'columns_speedup.tex')
+
+    start_block = \
+    '''
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    \SetCell[r=5]{{m}} {Scheduling}
+    '''
+
+    mid_block = \
+    '''
+    %%%%%%%%%%%%%
+    & {Iterations} &
+    {Val_1} &
+    {Val_2} &
+    {Val_3} &
+    {Val_4} &
+    {Val_8} \\\\
+    '''
+
+    end_block = \
+    '''
+    %%%%%%%%%%%%%
+    \hline
+    '''
+
+    tex_block = '\t'
+
+    ###################################
+    # Iterators
+    ###################################
+
+    iterations_start = 2500
+    schedules = ['static', 'dynamic', 'guided']
+    threadcount = [1, 2, 3, 4, 8]
+    chunks = ['default', '1']
+
+    ###################################
+    # Main logic
+    ###################################
+
+    method_dir = os.path.join('..', 'results', method)
+    base_file = os.path.join(method_dir, 'speedup')     # DOING IT ONLY FOR SPEEDUP
+
+    for schedule in schedules:
+        for chunk in chunks:
+
+            csv_file = base_file + '__' + schedule + '_' + chunk + '.csv'
+            if not os.path.exists(csv_file):
+                raise FileNotFoundError(f"csv file: {csv_file} not found")
+
+            if chunk == 'default' :
+                schedule_str = schedule
+            elif chunk == '1':
+                schedule_str = schedule + ', 1'
+            
+            tex_block += start_block.format(
+                Scheduling=schedule_str
+            ).strip()
+
+            tex_block += '\n\t'
+            
+            valuelist = csv_to_list(csv_file)
+
+            for value_row in valuelist:
+                tex_block += mid_block.format(
+                    # Iterations=string_to_power_of_ten(value_row[0]), # choose this for exponent notation
+                    Iterations=value_row[0], # choose this for full values
+                    Val_1=value_row[1],
+                    Val_2=value_row[2],
+                    Val_3=value_row[3],
+                    Val_4=value_row[4],
+                    Val_8=value_row[5]
+                ).strip()
+                tex_block += '\n\t'
+
+            tex_block += end_block.strip()
+            tex_block += '\n\t'
+    
+        with open(output_filename, 'w') as tex_file:
+            tex_file.write(tex_block)
