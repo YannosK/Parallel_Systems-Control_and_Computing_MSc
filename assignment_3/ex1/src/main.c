f643fdd7109df0ed5a61e27b6272ea5aad7a18f9
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2025-01-15 00:44:12 +0200

    Initial Changes
    - Take only two arguments from command line:
      1. number of generations,
      2. grid side size.
    - Remove the parallel version with OpenMP and leave only the serial one.
    - Make necessary changes to the Makefile to incorporate MPI.
    - Ignore files from .gitignore.

--- assignment_3/ex1/src/main.c
+++ assignment_3/ex1/src/main.c
@@ -1,4 +1,3 @@
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,25 +5,21 @@
 #include "timer.h"
 
 void argument_parse_error_message(char *program_name) {
-    printf("Usage: %s <generations> <grid> <mode> <threads>\n", program_name);
+    printf("Usage: %s <generations> <grid>\n", program_name);
     printf("\nArguments:\n");
     printf(" - generations: the number of generations to simulate.\n");
     printf(" - grid: the size of the grid (e.g. 10 for 10x10 grid).\n");
-    printf(" - mode: (0) serial or (1) parallel.\n");
-    printf(" - threads: the number of threads in parallel mode.\n");
 }
 
 int main(int argc, char *argv[]) {
     // Parse arguments
-    if(argc != 5) {
+    if(argc != 3) {
         argument_parse_error_message(argv[0]);
         return 1;
     }
 
     int generations = strtol(argv[1], NULL, 10);
     int grid = strtol(argv[2], NULL, 10);
-    int mode = strtol(argv[3], NULL, 10);
-    int threads = strtol(argv[4], NULL, 10);
 
     // Initialize state
     game_of_life_t gol;
@@ -52,15 +47,9 @@ int main(int argc, char *argv[]) {
     double start, end;
 
     // Execute the game of life
-    if(mode == 0) {
-        GET_TIME(start);
-        gol_execute(&gol, generations);
-        GET_TIME(end);
-    } else {
-        start = omp_get_wtime();
-        gol_execute_parallel(&gol, generations, threads);
-        end = omp_get_wtime();
-    }
+    GET_TIME(start);
+    gol_execute(&gol, generations);
+    GET_TIME(end);
 
     printf("Execution time: %.10fs\n", end - start);
 
