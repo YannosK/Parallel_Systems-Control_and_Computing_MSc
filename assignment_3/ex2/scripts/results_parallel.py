9c0ecdb39f9996b57b4b705299a31bd491f2b302
Ioannis Koutoulas <ea2300204@linux01.di.uoa.gr>
2025-01-18 13:15:21 +0200

    Saved results from DI

--- assignment_3/ex2/scripts/results_parallel.py
+++ assignment_3/ex2/scripts/results_parallel.py
@@ -128,7 +128,8 @@ if __name__ == "__main__":
    # Iterators
    #****************#
 
-   sizes = [8192, 16384, 32768] # problem size
+   sizes = [128, 256, 512, 1024, 2048, 4096] # problem size
+   # sizes = [8192, 16384, 32768] # problem size
    processescounts = [1, 4, 16, 64, 128] # MPI process count
    # processescounts = [1, 4, 8] # MPI process count
 
